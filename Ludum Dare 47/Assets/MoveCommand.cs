5d03117696184fa1145d6add0dd94fe275133bbb
vlandemart <vlandemart@gmail.com>
2020-10-03 14:12:16 +0300

    Command + ignore

---
+++ Ludum Dare 47/Assets/MoveCommand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MoveCommand : ICommand
+{
+    public Vector3 InputVector;
+    
+    public void Do()
+    {
+        //Move for InputVector
+    }
+
+    public void Undo()
+    {
+        //Move for -InputVector
+    }
+}
