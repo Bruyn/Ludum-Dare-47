ef43ca64e87309cc6772df61185b11ded7a9c0e2
Bruyn <bruyn777@gmail.com>
2020-10-03 13:30:48 +0300

    +Player Controller

---
+++ Ludum Dare 47/Assets/Scenes/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(horizontal, 0, vertical);
+    }
+}
