c2fa78678f8fac157d527f30f7c48094503644e7
Bruyn <bruyn777@gmail.com>
2020-10-03 13:55:05 +0300

    +update controller

--- Ludum Dare 47/Assets/Scenes/PlayerController.cs
+++ Ludum Dare 47/Assets/Scenes/PlayerController.cs
@@ -10,12 +10,30 @@ public class PlayerController : MonoBehaviour
     }
 
     public Rigidbody rb;
+    public float speed = 1;
+    public float jumptHeight = 5;
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        rb.velocity = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = rb.velocity;
+        velocity.x = horizontal * speed;
+        velocity.z = vertical * speed;
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            velocity.y += jumptHeight;
+        }
+
+
+        rb.velocity = velocity;
+    }
+
+    bool IsGrounded()
+    {
+        float distToGround = 0.5f;
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround);
     }
 }
