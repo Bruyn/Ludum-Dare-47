3749fdafe3669b9d5741b1c592f1aabb867d5ef2
vlandemart <vlandemart@vlandemart-pc>
2020-10-05 00:10:11 +0300

    Demo scene completed

---
+++ Ludum Dare 47/Assets/Scripts/Breakable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    [SerializeField] private string key;
+    
+    public void Break(string incomingKey)
+    {
+        if (incomingKey != key)
+            return;
+
+        gameObject.SetActive(false);
+        //we can add fancy particles later
+    }
+}
