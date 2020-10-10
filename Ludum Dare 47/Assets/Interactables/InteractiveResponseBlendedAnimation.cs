852a246b1c2c52bc13f099aec804cc8d3ad79b73
noxymu5 <54662724+noxymu5@users.noreply.github.com>
2020-10-10 19:55:22 +0300

    fixed interactibles
    change door and plate animation to blending, fixed plates

---
+++ Ludum Dare 47/Assets/Interactables/InteractiveResponseBlendedAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class InteractiveResponseBlendedAnimation : InteractiveResponse
+{
+    public float animationSpeed = 1f;
+    
+    private Animator _animator;
+    private float blendParam = .0f;
+
+    private int currIncrement = -1;
+    
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        blendParam = Mathf.Clamp(blendParam + animationSpeed * Time.deltaTime * currIncrement, 0, 1) ;
+        _animator.SetFloat("blendParam", blendParam);
+    }
+
+    public override void DoResponseAction()
+    {
+        currIncrement = 1;
+    }
+
+    public override void UndoResponseAction()
+    {
+        currIncrement = -1;
+    }
+
+    public override bool IsAvailable()
+    {
+        return true;
+    }
+}
